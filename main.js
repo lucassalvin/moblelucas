0ffae0da6c4a002100c0b9339220079bab7d0b36
lucassalvin <130079445+lucassalvin@users.noreply.github.com>
2023-09-28 13:46:19 -0300

    Add files via upload
    28/09

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play (); 
-}  
-document.querySelector('.tecla_pom'). onclick = tocaSomPom;
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas= document.querySelectorAll('.tecla');
+    
+let contador= 0;
+    
+while (contador<listaDeTeclas.length) {
+    
+    const tecla = listaDeTeclas[contador];
+    
+    const instrumento = tecla.classList [1];
+    
+    const idAudio =`#som_${instrumento}`;
+    
+        console.log (idAudio);
+    
+    tecla.onclick= function () {
+        tocaSom(idAudio);
+    }
+    contador=contador+1;
+    
+    console.log(contador);
+}
+     
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play (); 
-}  
-document.querySelector('.tecla_clap'). onclick = tocaSomClap;
+
+
+
+
